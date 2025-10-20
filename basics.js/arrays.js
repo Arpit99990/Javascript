b35b5de57b6599262fee4ebd805b8b38618bd1d1
Interviewbit <arpittiwari621@gmail.com>
2025-10-20 18:17:07 +0530

    js basics

---
+++ basics.js/arrays.js
@@ -0,0 +1,67 @@
+// const arr=[0,1,2,3,4]
+// console.log(arr);
+
+ //const myArr=new Array(1,2,3,4)
+// console.log(myArr);
+
+// myArr.push(5)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(0)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+//console.log(myArr.includes(6))
+
+// console.log("A",myArr);
+// const myn1=myArr.slice(1,3)
+// console.log(myn1);
+
+// console.log("B",myArr);
+// const myn2=myArr.splice(1,2)
+// console.log(myn2);
+
+// const myarr1=[1,2,3,4]
+// const myarr2=[5,6,7,8]
+
+// myarr1.push(myarr2)
+// console.log(myarr1);
+
+// const myarr3=myarr1.concat(myarr2)
+// console.log(myarr3);
+
+// const myarr4=[...myarr1,...myarr2]
+// console.log(myarr4);
+
+// const myarr5=[1,2,3,[7,8],9.4]
+// const boom=myarr5.flat()
+// console.log(boom);
+
+console.log(Array.isArray("Arpit"));
+console.log(Array.from("Arpit"));
+
+let marks1=23
+let marks2=99
+console.log(Array.of(marks1,marks2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
